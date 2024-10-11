623f1cefbd0d9845eadd3f6735fa26c80c41effa
luluzeraa <167870466+luluzeraa@users.noreply.github.com>
2024-10-11 08:59:08 -0300

    Subindo projeto padrão

---
+++ src/test/java/br/sesi/bank/bank_java_jbdc/BankJavaJbdcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.sesi.bank.bank_java_jbdc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankJavaJbdcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
